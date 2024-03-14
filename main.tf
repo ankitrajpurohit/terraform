458b8924713a703c0edcedf452497d4bd49f8695
ankitrajpurohit <asrajpurohit35@gmail.com>
2024-03-14 12:41:55 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ terraform {
 provider "datadog" {
   api_key   = "a299ddbcc0073d2fda2f9c40e930f765"
   app_key    = "ca1f7346daf084b864c7bcc4a8a3dd4255aa7378"
-  datadog_url = "https://api.datadoghq.com"  # Optional, defaults to above URL
 }
 # Define the Datadog monitor resource
 resource "datadog_monitor" "cpumonitor" {
