76d9b2ac2ad7abf12a59cb1e296e64e5f491e336
ankitrajpurohit <asrajpurohit35@gmail.com>
2024-03-14 12:39:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    datadog = {
+      source = "Datadog/datadog"
+   }
+  }
+}
+
+
 provider "datadog" {
   api_key   = "a299ddbcc0073d2fda2f9c40e930f765"
   app_key    = "ca1f7346daf084b864c7bcc4a8a3dd4255aa7378"
